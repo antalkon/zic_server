79680a8f1df30693f825f47f62039946c8ff0d34
Antipev Aleksei <87186332+antalkon@users.noreply.github.com>
2024-11-16 14:50:29 +0500

    MAIN

--- internal/models/computer.go
+++ internal/models/computer.go
@@ -7,7 +7,7 @@ type Computer struct {
 	RoomID uint64 `gorm:"not null" json:"room"`                     // RoomID больше не связан с таблицей Room
 	Name   string `gorm:"size:55;not null" json:"name"`
 	LIP    string `gorm:"size:255;not null;unique" json:"lip"`
-	PIP    string `gorm:"size:255;not null;unique" json:"pip"`
+	PIP    string `gorm:"size:255;not null" json:"pip"`
 	Status string `gorm:"size:255;default:'Н/Д...'" json:"status"`
 	On     bool   `gorm:"default:true" json:"on"`
 }
